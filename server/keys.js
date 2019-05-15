50c36108fa8bbabe89724c6a272545263ce6f60e
Rui Magalhaes <m.rui.s.magalhaes@gmail.com>
2019-05-15 12:59:17 +0100

    ...

---
+++ server/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser:process.env.PGUSER,
+  Host:process.env.HOST,
+  pgDatabase: process.env.DATABASE,
+  pg.
+};
